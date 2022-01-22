2a2bfed0dddb09cd1a5ebb2013defe1ce0c75df3
K SRI SAI MANI <srisaimanikovvada@gmail.com>
2022-01-22 19:25:18 +0530

    Update ArithmeticOperators.py

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -1,57 +1,16 @@
 #ARITHMETIC OPERATORS 
-#SET1
-a=5
-b=2
+
+a=input('Enter first value:')
+b=input('Enter second value:')
 c=a+b
 d=a-b
 e=a*b
 f=a/b
 g=a%b
 h=a**b
-print("The resultant values are ",c,d,e,f,g,h)
+print("The resultant values are "a,b,c,d,e,f,g,h)
 
-#OUTPUT1:The resultant values are  7 3 10 2.5 1 25
-
-
-
-#SET2
-a=10
-b=2
-c=a+b
-d=a-b
-e=a*b
-f=a/b
-g=a%b
-h=a**b
-print("The resultant values are ",c,d,e,f,g,h)
-
-#OUTPUT2:The resultant values are  12 8 20 5.0 0 100
-
-
-
-#SET3
-a=10
-b=5
-c=a+b
-d=a-b
-e=a*b
-f=a/b
-g=a%b
-h=a**b
-print("The resultant values are ",c,d,e,f,g,h)
-
-#OUTPUT:The resultant values are  15 5 50 2.0 0 100000
-
-
-#SET4
-a=102
-b=5
-c=a+b
-d=a-b
-e=a*b
-f=a/b
-g=a%b
-h=a**b
-print("The resultant values are ",c,d,e,f,g,h)
-
-#OUTPUT:The resultant values are  107 97 510 20.4 2 11040808032
+#OUTPUT1:The resultant values are 5 2 7 3 10 2.5 1 25
+#OUTPUT2:The resultant values are 6 2  12 8 20 5.0 0 100
+#OUTPUT:The resultant values are  10 5 15 5 50 2.0 0 100000
+#OUTPUT:The resultant values are  102 5 107 97 510 20.4 2 11040808032
