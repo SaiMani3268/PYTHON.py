1bdb7f3db7c9e68e8a5a01488eb7f9afbafd4e14
K SRI SAI MANI <srisaimanikovvada@gmail.com>
2022-01-24 22:32:33 +0530

    Add files via upload

---
+++ fn extension.py
@@ -0,0 +1,12 @@
+#FILENAME EXTENSION
+fun_name = input("Enter the filename:")
+x = fun_name.split(".")
+print("The extention of the file is:" + x[-1])
+
+#OUTPUT-1:
+#Enter the filename:area.py
+#The extention of the file is: py
+
+#OUTPUT-2:
+#Enter the filename:area.java
+#The extention of the file is: java
